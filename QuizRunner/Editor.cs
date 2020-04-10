f4331cfddce7c4080292036edb7b1e403b3334ab
Katerina Belousova <grantired.jpg@gmail.com>
2020-04-10 13:58:02 +0700

    Add properties of class

--- QuizRunner/Editor.cs
+++ QuizRunner/Editor.cs
@@ -8,5 +8,20 @@ namespace QuizRunner.Editor
 {
     class Editor
     {
+        private struct Answer
+        {
+            public string AnswerText;
+            public string[] Argument;
+        }
+        private struct Question
+        {
+            public string[] QuestionText;
+            public bool AnswType;
+            public Answer[] AnswArr;
+        }
+        private string Name;
+        private string[] Descrip = new string[0];
+        private Question[] ListOfQuestions = new Question[0];
     }
+
 }
