11c343a2d696fa33f093d76de0188a9fdae43fae
Mikhail 'stv233' Glazatov <me@stv233.pro>
2020-04-22 16:47:40 +0700

    Created a form for testing

---
+++ QuizRunner/Form4.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizRunner
+{
+    public partial class IfrTesting : Form
+    {
+        // Свойство формы, указывающее ращрешено ли её закрыться.
+        public bool CanClose;
+
+        // Свойство формы, указывающее были ли изменены данные, после открытия или создания.
+        public bool Changed;
+
+        // Свойство формы, указывающее, проходит ли на ней какая либо загрузка в данный момент.
+        public bool LoadingProcess;
+
+        public IfrTesting()
+        {
+            InitializeComponent();
+        }
+
+        private void IfrTesting_Load(object sender, EventArgs e)
+        {
+            var IttCreatorToolTip = new ToolTip();
+
+            /// Меню.
+            #region
+            // Панель меню.
+            var IpnMenu = new Panel
+            {
+                BackColor = Color.FromArgb(18, 136, 235),
+                Width = 60,
+                Height = this.ClientSize.Height,
+                Parent = this
+            };
+
+
+            // Кнопка открытия из файла.
+            var IpbOpen = new PictureBox
+            {
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = Properties.Resources.OpenPic,
+                Width = 50,
+                Height = 50,
+                Cursor = System.Windows.Forms.Cursors.Hand,
+                Parent = IpnMenu,
+                Left = 5,
+                Top = 5
+            };
+            IpbOpen.MouseEnter += MenuButtons_MouseEnter;
+            IpbOpen.MouseLeave += MenuButtons_MouseLeave;
+            //IpbOpen.Click += IpbOpen_Click;
+            IttCreatorToolTip.SetToolTip(IpbOpen, "Открыть...");
+
+            // Кнопка закрытия программы.
+            var IlbExit = new Label
+            {
+                AutoSize = false,
+                Width = IpnMenu.Width,
+                Height = IpnMenu.Width,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new Font("Verdana", 25, FontStyle.Bold),
+                ForeColor = Color.White,
+                Cursor = System.Windows.Forms.Cursors.Hand,
+                Parent = IpnMenu,
+                Top = IpnMenu.Height - IpnMenu.Width
+            };
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                IlbExit.Text = "X";
+            }
+            else
+            {
+                IlbExit.Text = "❌";
+            }
+            IlbExit.MouseEnter += IlbExit_MouseEnter;
+            IlbExit.MouseLeave += IlbExit_MouseLeave;
+            IlbExit.Click += IlbExit_Click;
+            IttCreatorToolTip.SetToolTip(IlbExit, "Закрыть " + AppDomain.CurrentDomain.FriendlyName.Substring(0,
+                    AppDomain.CurrentDomain.FriendlyName.Length - 4));
+
+            // Кнопка выхода в меню.
+            var IpbBack = new PictureBox
+            {
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = Properties.Resources.BackPic,
+                Width = 50,
+                Height = 50,
+                Cursor = System.Windows.Forms.Cursors.Hand,
+                Parent = IpnMenu,
+                Left = 5,
+            };
+            IpbBack.Top = IlbExit.Top - IpbBack.Height - 15;
+            IpbBack.MouseEnter += MenuButtons_MouseEnter;
+            IpbBack.MouseLeave += MenuButtons_MouseLeave;
+            IpbBack.Click += IpbBack_Click;
+            IttCreatorToolTip.SetToolTip(IpbBack, "Вернуться в меню");
+            #endregion
+        }
+
+        /// События основных графических элементов.
+        #region
+        // Вход курсора в область одной из кнопок меню.
+        private void MenuButtons_MouseEnter(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).Width += 4;
+            ((PictureBox)sender).Height += 4;
+            ((PictureBox)sender).Left -= 2;
+            ((PictureBox)sender).Top -= 2;
+        }
+
+        // Покидание курсором области одной из кнопок меню.
+        private void MenuButtons_MouseLeave(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).Width -= 4;
+            ((PictureBox)sender).Height -= 4;
+            ((PictureBox)sender).Left += 2;
+            ((PictureBox)sender).Top += 2;
+        }
+
+        private void IlbExit_MouseEnter(object sender, EventArgs e)
+        {
+            ((Label)sender).Font = new Font("Verdana", 30, FontStyle.Bold);
+            ((Label)sender).BackColor = Color.Red;
+        }
+
+        private void IlbExit_MouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).Font = new Font("Verdana", 25, FontStyle.Bold);
+            ((Label)sender).BackColor = Color.Transparent;
+        }
+
+        private void IlbExit_Click(object sender, EventArgs e)
+        {
+            CanClose = true;
+            Application.Exit();
+        }
+
+        private void IpbBack_Click(object sender, EventArgs e)
+        {
+            CanClose = true;
+            new IfrStartPage().Show();
+            Close();
+        }
+
+        #endregion
+    }
+}
