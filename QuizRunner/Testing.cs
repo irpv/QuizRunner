396ce133ae6bc494315af2b386db526cfbcd7bf7
ilyarpv <ilyarpv@gmail.com>
2020-05-05 11:18:38 +0700

    Basic program structure

---
+++ QuizRunner/Testing.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace QuizRunner.Testing
+{
+    public class Testing
+    {
+        
+    }
+}
