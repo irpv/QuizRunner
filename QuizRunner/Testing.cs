6566cd55325aa8b9ee598ae12669618d7ce16d78
ilyarpv <ilyarpv@gmail.com>
2020-05-05 11:33:06 +0700

    Add function to remove all string spaces

--- QuizRunner/Testing.cs
+++ QuizRunner/Testing.cs
@@ -8,6 +8,11 @@ namespace QuizRunner.Testing
 {
     public class Testing
     {
-        
+        private int _closingBracket = 0;
+
+        public string SimplifyArg(string arg)
+        {
+            return Regex.Replace(arg, @"\s+", "");
+        }
     }
 }
