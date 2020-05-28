18e93f6f24269df90a9030a4d2dd16a49ffd8740
ilyarpv <ilyarpv@gmail.com>
2020-05-28 18:22:09 +0700

    Add functional to save test's name, description and number of questions

--- QuizRunner/SaveArgs.cs
+++ QuizRunner/SaveArgs.cs
@@ -24,9 +24,25 @@ namespace QuizRunner.SaveArgs
         private string[] Descrip = new string[0];
         private Question[] ListOfQuestions = new Question[0];
 
-		public void Save(string path)
-		{
+        public void Save(string path)
+        {
+            File.Create(path).Close();
+            var SW = File.AppendText(path);
 
-		}
+            // Записывает имя теста
+            SW.WriteLine(Name);
+
+            // Записывает размер описания
+            SW.WriteLine(Descrip.Length.ToString());
+            SW.Close();
+
+            // Записывает текст описания
+            File.AppendAllLines(path, Descrip);
+            SW = File.AppendText(path);
+
+            // Записывает количество вопросов
+            SW.WriteLine(ListOfQuestions.Length.ToString());
+
+        }
     }
 }
