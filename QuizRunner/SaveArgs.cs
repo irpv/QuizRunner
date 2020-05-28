41c3415143508439ce7678797b81145f52003504
ilyarpv <ilyarpv@gmail.com>
2020-05-28 18:16:49 +0700

    Add necessary structs

--- QuizRunner/SaveArgs.cs
+++ QuizRunner/SaveArgs.cs
@@ -4,6 +4,24 @@ namespace QuizRunner.SaveArgs
 {
     public class SaveArgs
     {
+        private struct Answer
+        {
+            public string AnswerText;
 
+            // Хранят строки для расчета статистики.
+            public string[] Argument;
+            public bool Checked;
+        }
+
+        private struct Question
+        {
+            public string[] QuestionText;
+            public bool AnswType;
+            public Answer[] AnswArr;
+        }
+
+        private string Name;
+        private string[] Descrip = new string[0];
+        private Question[] ListOfQuestions = new Question[0];
     }
 }
