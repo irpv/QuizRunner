f67a4be58ca7b6d2611df3cfc5e99d610219a93a
ilyarpv <ilyarpv@gmail.com>
2020-05-29 00:01:39 +0700

    Add unit tests for Testing.cs

---
+++ TestTesting/TestTesting.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using QuizRunner.Testing;
+using System.Collections.Generic;
+
+namespace TestTesting
+{
+    public class Tests
+    {
+        public int _closingBracket = 0;
+
+        [Test]
+        public void ToSimplifyArg()
+        {
+            string arg = "[test] = [test] + 1";
+            string actual = "[test]=[test]+1";
+
+            Testing t = new Testing();
+
+            Assert.AreEqual(t.SimplifyArg(arg), actual);
+        }
+
+        [Test]
+        public void getArg()
+        {
+            string arg = "[test] = [test] + 1";
+            string actual = "test ";
+
+            Testing t = new Testing();
+
+            Assert.AreEqual(t.GetArgumentName(arg), actual);
+        }
+
+        /*public void GetCmpte()
+        {
+            string arg;
+            Dictionary<string, double> D;
+            object actual;
+
+            Testing t = new Testing();
+
+            Assert.AreEqual(t.GetCompute(arg, D), actual);
+
+        }*/
+    }
+}
