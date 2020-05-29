e364a7ba484fa5cdb5b3936fb9477bc6cf10176a
Mikhail 'stv233' Glazatov <glazatov.mihail@gmail.com>
2020-05-29 15:49:59 +0700

    Add an icon for the results viewing form

--- QuizRunner/Form5.Designer.cs
+++ QuizRunner/Form5.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IfrResult));
             this.SuspendLayout();
             // 
             // IfrResult
@@ -36,6 +37,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 738);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "IfrResult";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Результаты";
