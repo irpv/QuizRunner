82cb9cf1aa4d7fe68b0b18b2c7733c1e1c01409b
irpv <49901453+irpv@users.noreply.github.com>
2020-05-29 13:51:58 +0700

    Update TestTesting.cs

--- TestTesting/TestTesting.cs
+++ TestTesting/TestTesting.cs
@@ -30,16 +30,18 @@ namespace TestTesting
             Assert.AreEqual(t.GetArgumentName("[] = [] + 1"), " ");
         }
 
-        /*[Test]
+        [Test]
         public void GetCmpte()
         {
-            string arg;
-            Dictionary<string, double> D;
-            object actual;
+            string arg = "[test] = [test] + 1";
+            var dict = new Dictionary<string, double>();
+            dict.Add("test", 1);
+
+            object actual = 2;
 
             Testing t = new Testing();
 
-            Assert.AreEqual(t.GetCompute(arg, D), actual);
-        }*/
+            Assert.AreEqual(t.GetCompute(arg, dict), actual);
+        }
     }
 }
